9709701f5cbef0f072f97dd71d495beb1dfcc07d
Lazar Antonijevic <lazarsa@aia.ac>
2024-09-20 12:30:45 +0200

    Added testing retries & cross-browser support(headless and headed)

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,13 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://the-internet.herokuapp.com'
+    baseUrl: 'https://the-internet.herokuapp.com',
+
   },
+  retries: {
+    //Configurin retry attempts for 'cypress run'
+    runMode: 2,
+    //Configuring retry attempts for 'cypress open'
+    openMode: 1
+  }
 });
